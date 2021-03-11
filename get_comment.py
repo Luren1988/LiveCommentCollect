a0ad110766b1f5dc49872edb773c44e3a54af2a5
Luren1988 <19luren88@gmail.com>
2021-03-12 00:09:48 +0900

    リファクタリング

--- get_comment.py
+++ get_comment.py
@@ -2,73 +2,85 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-#取得間隔
-COLLECT_INTERVAL = 10
-#driverのパス
-driver_path = 'driver/chromedriver'
-#取得するyoutubeのライブ
-target_url = ''
-#結果ストレージ [{id:{"message":message,"timestamp":timestamp,"author":author}},....]
-dump_list = []
 
-def getInnerHTML(content,target):
-    result = ""
-    try:
-        result = content.find_element_by_id(target).get_attribute('innerHTML')
-    except:
+class CommentCollect:
+
+    def __init__(self):
+        self.COLLECT_INTERVAL = 10
+        self.DRIVER_PATH = 'driver/chromedriver'
+        self.TARGET_URL = ''
+        self.dump_list = []
+        self.driver = webdriver.Chrome(self.DRIVER_PATH)
+    
+    #def __del__(self):
+    #    self.driver.quit()
+     
+    def __GetInnerHTML(self,content,target):
         result = ""
-    return result
+        try:
+            result = content.find_element_by_id(target).get_attribute('innerHTML')
+        except:
+            result = ""
+        return result
 
-def getText(content,target):
-    result = ""
-    try:
-        result = content.find_element_by_id(target).text
-    except:
+    def __GetText(self,content,target):
         result = ""
-    return result       
+        try:
+            result = content.find_element_by_id(target).text
+        except:
+            result = ""
+        return result       
 
-def update_comment(contents):
-
-    if len(contents) > 0:
-        for content in contents:
+    def Prepare(self):
+        if self.TARGET_URL:
             try:
-                unique_id = content.get_attribute('id')
-                check_duplicate = [ d.get(unique_id) for d in dump_list if d.get(unique_id)]
-                if len(check_duplicate) == 0:
-                    timestamp = getInnerHTML(content,'timestamp')
-                    message = getText(content,'message')
-                    author = getText(content,'author-name')
-                    dump_list.append({unique_id:{"timestamp":timestamp,"author":author,"message":message}})
+                self.driver.get(self.TARGET_URL)
+                time.sleep(5)
 
-                    print(timestamp + " - " +  author + " - " + message  )
+                iframe = self.driver.find_element_by_xpath('//*[@id="chatframe"]')
+                self.driver.switch_to.frame(iframe)
+            except:
+                print("ドライバーが設定できませんでした。")
+        else:
+            print('urlがセットされていません')
+
+    def UpdateComment(self,contents):
+
+        if len(contents) > 0:
+            for content in contents:
+                try:
+                    unique_id = content.get_attribute('id')
+                    check_duplicate = [ d.get(unique_id) for d in self.dump_list if d.get(unique_id)]
+                    if len(check_duplicate) == 0:
+                        timestamp = self.__GetInnerHTML(content,'timestamp')
+                        message = self.__GetText(content,'message')
+                        author = self.__GetText(content,'author-name')
+                        self.dump_list.append({unique_id:{"timestamp":timestamp,"author":author,"message":message}})
+
+                        print(timestamp + " - " +  author + " - " + message  )
+
+                except Exception as e:
+                    print(e)
+    
+    def CheckComment(self):
+        while True:
+            try:
+                items = self.driver.find_elements_by_xpath('//*[@id="items"]')
+                if len(items) > 0 :
+                    contents = items[1].find_elements_by_tag_name('yt-live-chat-text-message-renderer')
+                    self.UpdateComment(contents)
+                else:
+                    print('no element found')
 
             except Exception as e:
                 print(e)
 
+            time.sleep(self.COLLECT_INTERVAL)
+    
 
-#[TODO]:headless chrome 対応 
-#options = Options()
-#options.add_argument('--headless')
-
-
-driver = webdriver.Chrome(driver_path)
-driver.get(target_url)
-#起動待ち時間
-time.sleep(5)
-
-iframe = driver.find_element_by_xpath('//*[@id="chatframe"]')
-driver.switch_to.frame(iframe)
-
-while True:
-    try:
-        items = driver.find_elements_by_xpath('//*[@id="items"]')
-        contents = items[1].find_elements_by_tag_name('yt-live-chat-text-message-renderer')
-        update_comment(contents)
-         
-    except Exception as e:
-        print(e)
-
-    time.sleep(COLLECT_INTERVAL)
-
-driver.quit()
-
+if __name__=='__main__':
+    CC = CommentCollect()
+    CC.TARGET_URL = 'https://www.youtube.com/watch?v=RDiarjPxQiQ'
+    
+    CC.Prepare()
+    CC.CheckComment()
